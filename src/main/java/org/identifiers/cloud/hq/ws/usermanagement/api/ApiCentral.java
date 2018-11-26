4b912d2c7c752f79fc4d3f844b204a3162afb1a5
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-26 13:21:43 +0000

    API Central

---
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/api/ApiCentral.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.hq.ws.usermanagement.api;
+
+/**
+ * Project: user-management
+ * Package: org.identifiers.cloud.hq.ws.usermanagement.api
+ * Timestamp: 2018-11-26 13:19
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ApiCentral {
+    // Current API Version flag
+    public static final String apiVersion = "1.0";
+}
