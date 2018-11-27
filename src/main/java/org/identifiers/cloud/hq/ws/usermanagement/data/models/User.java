a7087722f9574bdf935e356839d30029e6c7319c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-27 10:31:32 +0000

    association between users and their roles

--- src/main/java/org/identifiers/cloud/hq/ws/usermanagement/data/models/User.java
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/data/models/User.java
@@ -2,10 +2,13 @@ package org.identifiers.cloud.hq.ws.usermanagement.data.models;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotNull;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Project: user-management
@@ -26,4 +29,5 @@ public class User {
     // By default, a newly created account is neither verified nor active
     private boolean verified = false;
     private boolean active = false;
+    @DBRef private List<Role> roles = new ArrayList<>();
 }
