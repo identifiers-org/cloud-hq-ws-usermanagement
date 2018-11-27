18ab9e7a76bdf1cdc526eddca6f83fcba2573c0d
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-27 10:46:37 +0000

    account verification token

---
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/data/models/AccountVerificationToken.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.usermanagement.data.models;
+
+/**
+ * Project: user-management
+ * Package: org.identifiers.cloud.hq.ws.usermanagement.data.models
+ * Timestamp: 2018-11-27 10:46
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class AccountVerificationToken {
+}
