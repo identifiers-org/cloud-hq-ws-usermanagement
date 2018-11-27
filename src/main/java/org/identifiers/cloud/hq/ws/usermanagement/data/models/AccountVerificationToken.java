aa8e361e25c8b8910b620986472d09c5df388bb7
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-27 11:13:39 +0000

    default expiry date for authorization tokens

--- src/main/java/org/identifiers/cloud/hq/ws/usermanagement/data/models/AccountVerificationToken.java
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/data/models/AccountVerificationToken.java
@@ -3,6 +3,7 @@ package org.identifiers.cloud.hq.ws.usermanagement.data.models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.sql.Timestamp;
 import java.util.UUID;
 
 /**
@@ -17,6 +18,11 @@ import java.util.UUID;
  */
 @Document
 public class AccountVerificationToken {
+    // 48 hours is the default expiry date for an authorization token
+    public static final long VERIFICATION_TOKEN_TTL_MILLIS = 48 * 3600 * 1000;
     // This makes sure the token is there when dealing with objects of this class
     @Id private String id = UUID.randomUUID().toString();
+    private Timestamp created = new Timestamp(System.currentTimeMillis());
+    private Timestamp expiryDate = new Timestamp(created.getTime() + AccountVerificationToken.VERIFICATION_TOKEN_TTL_MILLIS);
+    
 }
