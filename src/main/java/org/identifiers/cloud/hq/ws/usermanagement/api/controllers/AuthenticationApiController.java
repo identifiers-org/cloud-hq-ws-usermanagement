c8491798f4067d030ac2aa4341e5f1a8bb15a985
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-09 15:58:07 +0000

    authentication api controller, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/api/controllers/AuthenticationApiController.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.usermanagement.api.controllers;
+
+/**
+ * Project: user-management
+ * Package: org.identifiers.cloud.hq.ws.usermanagement.api.controllers
+ * Timestamp: 2019-01-09 15:57
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class AuthenticationApiController {
+}
