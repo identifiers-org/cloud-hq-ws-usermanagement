fbb80cd9bfa47e763c9be99e5a7afa6171f5b02b
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-09 10:05:20 +0000

    refactoring with project lombok

--- src/main/java/org/identifiers/cloud/hq/ws/usermanagement/data/models/Role.java
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/data/models/Role.java
@@ -4,8 +4,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.math.BigInteger;
-
 /**
  * Project: user-management
  * Package: org.identifiers.cloud.hq.ws.usermanagement.data.models
@@ -21,31 +19,4 @@ public class Role {
     @Id private String id;
     @Indexed(unique = true) private String name;
     private String description;
-
-    public BigInteger getId() {
-        return id;
-    }
-
-    public Role setId(BigInteger id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Role setName(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Role setDescription(String description) {
-        this.description = description;
-        return this;
-    }
 }
