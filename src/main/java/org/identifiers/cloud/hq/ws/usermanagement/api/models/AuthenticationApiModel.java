98072eeed47c9b598f17996e62aa6bbc42483036
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-09 15:58:24 +0000

    authentication api model, annotations

--- src/main/java/org/identifiers/cloud/hq/ws/usermanagement/api/models/AuthenticationApiModel.java
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/api/models/AuthenticationApiModel.java
@@ -1,5 +1,7 @@
 package org.identifiers.cloud.hq.ws.usermanagement.api.models;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Project: user-management
  * Package: org.identifiers.cloud.hq.ws.usermanagement.api.models
@@ -8,5 +10,6 @@ package org.identifiers.cloud.hq.ws.usermanagement.api.models;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Component
 public class AuthenticationApiModel {
 }
