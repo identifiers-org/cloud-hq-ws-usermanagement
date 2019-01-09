77c6af943b6b49c0f69f235d1e2794d56fc2a02e
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-09 15:57:29 +0000

    authentication api model, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/api/models/AuthenticationApiModel.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.usermanagement.api.models;
+
+/**
+ * Project: user-management
+ * Package: org.identifiers.cloud.hq.ws.usermanagement.api.models
+ * Timestamp: 2019-01-09 15:56
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class AuthenticationApiModel {
+}
