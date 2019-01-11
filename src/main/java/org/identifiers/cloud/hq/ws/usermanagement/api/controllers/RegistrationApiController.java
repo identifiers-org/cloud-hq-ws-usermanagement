84e4ead474ab2904352e65680f35c2fe651f96ba
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-11 11:33:24 +0000

    registration api contoller, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/api/controllers/RegistrationApiController.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.usermanagement.api.controllers;
+
+/**
+ * Project: user-management
+ * Package: org.identifiers.cloud.hq.ws.usermanagement.api.controllers
+ * Timestamp: 2019-01-11 11:33
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class RegistrationApiController {
+}
