f1dbd675a20f5456a696175278b6763d5125842a
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-11 11:42:33 +0000

    user registration request model

---
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/api/data/models/UserRegistrationRequestModel.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.usermanagement.api.data.models;
+
+/**
+ * Project: user-management
+ * Package: org.identifiers.cloud.hq.ws.usermanagement.api.data.models
+ * Timestamp: 2019-01-11 11:42
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class UserRegistrationRequestModel {
+}
