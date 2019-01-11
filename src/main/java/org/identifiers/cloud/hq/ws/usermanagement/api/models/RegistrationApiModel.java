4fdfd6e040450cd8e9a19036d6d59de3cba77c13
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-11 11:35:16 +0000

    registration api model

---
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/api/models/RegistrationApiModel.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.usermanagement.api.models;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: user-management
+ * Package: org.identifiers.cloud.hq.ws.usermanagement.api.models
+ * Timestamp: 2019-01-11 11:35
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class RegistrationApiModel {
+}
