a7216b6b7fa13df95e79aa70c63eed714126c62e
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-12 11:37:09 +0000

    InMemoryAuthenticationConfiguration, testing step by step how authentication works in Spring Security

---
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/configuration/InMemoryAuthenticationConfiguration.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.usermanagement.configuration;
+
+/**
+ * Project: user-management
+ * Package: org.identifiers.cloud.hq.ws.usermanagement.configuration
+ * Timestamp: 2019-01-12 11:36
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class InMemoryAuthenticationConfiguration {
+}
