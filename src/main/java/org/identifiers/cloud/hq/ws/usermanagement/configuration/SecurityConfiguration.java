e992c5b532d91fb405c7a71b58ddca355f05fc15
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-12 10:56:40 +0000

    building security configuration

---
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/configuration/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.usermanagement.configuration;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Project: user-management
+ * Package: org.identifiers.cloud.hq.ws.usermanagement.configuration
+ * Timestamp: 2019-01-12 10:56
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    // TODO
+}
