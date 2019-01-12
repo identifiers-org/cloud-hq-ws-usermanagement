8eabec390c26c063a9aaee3f951aa5b9f27861b7
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-12 11:38:55 +0000

    using in memory user details service

--- src/main/java/org/identifiers/cloud/hq/ws/usermanagement/configuration/InMemoryAuthenticationConfiguration.java
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/configuration/InMemoryAuthenticationConfiguration.java
@@ -1,5 +1,10 @@
 package org.identifiers.cloud.hq.ws.usermanagement.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 /**
  * Project: user-management
  * Package: org.identifiers.cloud.hq.ws.usermanagement.configuration
@@ -8,5 +13,11 @@ package org.identifiers.cloud.hq.ws.usermanagement.configuration;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Configuration
 public class InMemoryAuthenticationConfiguration {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager();
+    }
 }
