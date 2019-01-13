d541098c31f3ab060eac68f5476bb1b6157646c4
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-13 13:51:59 +0000

    custom user details model

---
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/authentication/models/UserDetailsModel.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.hq.ws.usermanagement.authentication.models;
+
+import org.identifiers.cloud.hq.ws.usermanagement.data.models.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * Project: user-management
+ * Package: org.identifiers.cloud.hq.ws.usermanagement.authentication.models
+ * Timestamp: 2019-01-13 13:49
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class UserDetailsModel extends User implements UserDetails {
+    // TODO
+}
--- src/main/java/org/identifiers/cloud/hq/ws/usermanagement/authentication/services/MongoUserDetailsService.java
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/authentication/services/MongoUserDetailsService.java
@@ -28,6 +28,8 @@ public class MongoUserDetailsService implements UserDetailsService {
         if (user == null) {
             throw new UsernameNotFoundException("User " + s + " NOT FOUND");
         }
+
+        UserDetails
         // TODO
         return null;
     }
