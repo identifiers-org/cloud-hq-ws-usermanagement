540479e51c196421c06afafef7c27f08de134be0
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-13 12:56:02 +0000

    main findBy method in the repository

--- src/main/java/org/identifiers/cloud/hq/ws/usermanagement/data/repositories/UserRepository.java
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/data/repositories/UserRepository.java
@@ -12,6 +12,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * ---
  */
 public interface UserRepository extends MongoRepository<User, String> {
-    // TODO
     User findByEmail(String email);
 }
