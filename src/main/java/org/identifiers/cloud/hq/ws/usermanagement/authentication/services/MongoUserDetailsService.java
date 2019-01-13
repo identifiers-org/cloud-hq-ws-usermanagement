3090ecbcf560d1e246563b9d4640e48c3046b41f
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-13 13:14:49 +0000

    mongo user details service, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/authentication/services/MongoUserDetailsService.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.hq.ws.usermanagement.authentication.services;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Project: user-management
+ * Package: org.identifiers.cloud.hq.ws.usermanagement.authentication.services
+ * Timestamp: 2019-01-13 13:14
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class MongoUserDetailsService implements UserDetailsService {
+}
