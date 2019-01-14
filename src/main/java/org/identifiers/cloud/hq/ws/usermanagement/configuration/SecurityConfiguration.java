8b7ae93708d95adf7d4803b7af84c24c736b3d55
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-14 12:52:33 +0000

    omg, I forgot to actually return the user details object

--- src/main/java/org/identifiers/cloud/hq/ws/usermanagement/configuration/SecurityConfiguration.java
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/configuration/SecurityConfiguration.java
@@ -1,12 +1,9 @@
 package org.identifiers.cloud.hq.ws.usermanagement.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Project: user-management
@@ -21,11 +18,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // TODO
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // HTTP Basic based authentication
