4e2a0e64c121365065be6247bc392f4d061ce223
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-15 10:45:32 +0000

    configuration class for development

---
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/configuration/DevelopmentConfiguration.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.usermanagement.configuration;
+
+/**
+ * Project: user-management
+ * Package: org.identifiers.cloud.hq.ws.usermanagement.configuration
+ * Timestamp: 2019-01-15 10:41
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class DevelopmentConfiguration {
+}
