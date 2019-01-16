8b75b589683f07fb6aef2f41b4e8290a2b471c87
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-16 11:45:11 +0000

    let's have a look at the principal

--- src/main/java/org/identifiers/cloud/hq/ws/usermanagement/api/controllers/AuthenticationApiController.java
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/api/controllers/AuthenticationApiController.java
@@ -23,8 +23,8 @@ public class AuthenticationApiController {
     // TODO - Mark this endpoint as not authenticated
     @GetMapping("/unprotected")
     public @ResponseBody
-    String unprotectedEndpoint() {
-        return "This is the unprotected endpoint";
+    String unprotectedEndpoint(Principal principal) {
+        return "This is the unprotected endpoint - " + principal;
     }
 
     @GetMapping("/protected")
