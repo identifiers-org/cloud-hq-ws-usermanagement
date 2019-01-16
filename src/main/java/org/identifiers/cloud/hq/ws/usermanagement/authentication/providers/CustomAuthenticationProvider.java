1f02471c36854e760051987ad7eb63ec954eb377
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-16 09:06:16 +0000

    custom authentication provider for finding out how to use multiple authentication mechanisms

---
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/authentication/providers/CustomAuthenticationProvider.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.usermanagement.authentication.providers;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: user-management
+ * Package: org.identifiers.cloud.hq.ws.usermanagement.authentication.providers
+ * Timestamp: 2019-01-16 09:04
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class CustomAuthenticationProvider {
+}
