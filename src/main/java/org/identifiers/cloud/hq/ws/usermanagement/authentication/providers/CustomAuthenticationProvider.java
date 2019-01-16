7233ac935e9e41ed22d4b6c35a15530856afee44
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-16 09:28:23 +0000

    custom authentication provider skeleton

--- src/main/java/org/identifiers/cloud/hq/ws/usermanagement/authentication/providers/CustomAuthenticationProvider.java
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/authentication/providers/CustomAuthenticationProvider.java
@@ -1,5 +1,9 @@
 package org.identifiers.cloud.hq.ws.usermanagement.authentication.providers;
 
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,5 +15,14 @@ import org.springframework.stereotype.Component;
  * ---
  */
 @Component
-public class CustomAuthenticationProvider {
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return aClass.equals(UsernamePasswordAuthenticationToken.class);
+    }
 }
