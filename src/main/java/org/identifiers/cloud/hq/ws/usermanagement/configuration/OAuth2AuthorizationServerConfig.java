3d91e8503b3e69dc21acac1b3a9f8ea04dacc9c9
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-16 12:18:16 +0000

    I need to configure a client details service

--- src/main/java/org/identifiers/cloud/hq/ws/usermanagement/configuration/OAuth2AuthorizationServerConfig.java
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/configuration/OAuth2AuthorizationServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
@@ -36,6 +37,12 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
         endpoints.tokenStore(tokenStore())
                 .accessTokenConverter(accessTokenConverter())
                 .authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        // TODO
+        super.configure(clients);
     }
 
     @Override
