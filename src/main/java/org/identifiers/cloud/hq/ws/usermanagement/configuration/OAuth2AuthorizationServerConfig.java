40839fa3be54a3a09338c7774e98261625b26542
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-16 10:07:04 +0000

    Let's try to add JWT

---
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/configuration/OAuth2AuthorizationServerConfig.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.usermanagement.configuration;
+
+/**
+ * Project: user-management
+ * Package: org.identifiers.cloud.hq.ws.usermanagement.configuration
+ * Timestamp: 2019-01-16 10:01
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class OAuth2AuthorizationServerConfig {
+}
