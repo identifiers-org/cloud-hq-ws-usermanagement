2fb4561b402c3ae24c65e2fb9abac9ac1b604537
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-16 11:23:01 +0000

    open access to the authentication manager bean

--- src/main/java/org/identifiers/cloud/hq/ws/usermanagement/configuration/SecurityConfiguration.java
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package org.identifiers.cloud.hq.ws.usermanagement.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -41,6 +42,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
