e24a6c5d2ff379f559f7a6554eaeee7bbce40f38
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-16 10:44:30 +0000

    the dependencies are not working

--- src/main/java/org/identifiers/cloud/hq/ws/usermanagement/configuration/OAuth2AuthorizationServerConfig.java
+++ src/main/java/org/identifiers/cloud/hq/ws/usermanagement/configuration/OAuth2AuthorizationServerConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
  * ---
  */
 @Configuration
-@EnableAuthorizationServer
 public class OAuth2AuthorizationServerConfig {
     // TODO
 }
